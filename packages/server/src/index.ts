62b2ba997f6bdf836f710b9a46a9eba44764e8dd
mikethehud <mail@mikehudson.de>
2019-04-17 00:20:59 +1200

    Basic hello world example

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,7 @@
-console.log("hi");
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.json({ message: "hello world" }));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
